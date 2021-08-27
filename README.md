bc4be5a6374fb1eec4c1ed946ddc9f2bc3e10ad2
JeuriMorel <rikonikage@yahoo.com>
2021-08-27 18:22:19 -0400

    all pages ready

---
+++ README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - myteam website
+
+![Design preview for the myteam website coding challenge](./preview.jpg)
+
+This is a solution to the [myteam website challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/myteam-multipage-website-mxlEauvW). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+-   [Overview](#overview)
+    -   [The challenge](#the-challenge)
+    -   [Links](#links)
+-   [My process](#my-process)
+    -   [Built with](#built-with)
+    -   [Continued development](#continued-development)
+-   [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+-   View the optimal layout for each page depending on their device's screen size
+-   See hover states for all interactive elements throughout the site
+-   See the correct content for each team member on the About page when the `+` icon is clicked
+-   Receive an error message when the contact form is submitted if:
+    -   The `Name`, `Email Address` or `Message` fields are empty should show "This field is required"
+
+### Links
+
+-   Solution URL: [Add solution URL here](https://your-solution-url.com)
+-   Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+-   Semantic HTML5 markup
+-   Flexbox
+-   CSS Grid
+-   Mobile-first workflow
+-   Sass
+-   Vanilla JS
+
+### Continued development
+
+-   BEM
+-   Accessibility
+
+## Author
+
+-   Frontend Mentor - [@JeuriMorel](https://www.frontendmentor.io/profile/JeuriMorel)
+-   Twitter - [@codeToPerdition](https://twitter.com/codeToPerdition)
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,13 @@
 // DOM
-const hamburger = document.querySelector('.hamburger')
-const body = document.querySelector('body')
-const btns = document.querySelectorAll('[data-btn]')
-const cards = Array.from(document.querySelectorAll('[data-card]'))
-
+const hamburger = document.querySelector(".hamburger");
+const body = document.querySelector("body");
+const btns = document.querySelectorAll("[data-btn]");
+const cards = Array.from(document.querySelectorAll("[data-card]"));
+const form = document.querySelector("form");
+const nameField = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const submitBtn = document.querySelector('button[type="submit"]');
 
 const hamSource = {
     false: "./assets/icon-hamburger.svg",
@@ -13,21 +17,74 @@ const hamSource = {
 // FUNCTIONS
 
 function handleHam() {
-    hamburger.classList.toggle('open')
-    body.classList.toggle('open')
+    hamburger.classList.toggle("open");
+    body.classList.toggle("open");
     let isOpen = hamburger.classList.contains("open");
 
-    hamburger.src = hamSource[isOpen]
+    hamburger.src = hamSource[isOpen];
 }
 
 function handleFlip(e) {
     let name = e.currentTarget.dataset.btn;
-    let target = cards.find(card => card.dataset.card === name)
-    
-    target.classList.toggle('flipped')
+    let target = cards.find((card) => card.dataset.card === name);
+
+    target.classList.toggle("flipped");
+}
+
+function handleSubmit() {
+    form.reset();
+    form.classList.add("success");
+    setTimeout(() => form.classList.remove("success"), 1000);
+}
+
+function validateSubmit() {
+    if (
+        !validateString(nameField.value, nameField) ||
+        !validateEmail() ||
+        !validateString(message.value, message)
+    ) {
+        submitBtn.disabled = "disabled";
+        return;
+    }
+    submitBtn.disabled = false;
+}
+
+function validateString(input, field) {
+    if (input) {
+        field.classList.remove("error");
+        return true;
+    }
+    field.classList.add("error");
+    return false;
+}
+
+function checkInput(e) {
+    validateString(e.target.value, e.target);
+    validateSubmit();
+}
+
+function validateEmail() {
+    let mailRegex =
+        /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    if (mailRegex.test(email.value)) {
+        email.classList.remove("error");
+        return true;
+    }
+
+    email.classList.add("error");
+    return false;
 }
 
 //EVENT LISTENERS
 
-hamburger?.addEventListener('click', handleHam)
-btns?.forEach(btn => btn.addEventListener('click', handleFlip))
+hamburger?.addEventListener("click", handleHam);
+btns?.forEach((btn) => btn.addEventListener("click", handleFlip));
+submitBtn?.addEventListener("mouseenter", validateSubmit);
+form?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleSubmit();
+});
+nameField?.addEventListener("input", checkInput);
+message?.addEventListener("input", checkInput);
+email?.addEventListener("input", validateSubmit);
